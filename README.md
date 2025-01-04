dfe73bb510f608f2a00fe71b3f17ccffc5783f77
Abin Thomas <followgamer102@gmail.com>
2025-01-04 17:20:55 +0530

    fix:updated ui

--- README.md
+++ README.md
@@ -1,52 +1,36 @@
-<div align="center">
-  <img src="https://media1.tenor.com/m/3yyytNGYC7cAAAAC/empty-tank.gif" alt="FuelMate Logo" wdith=90px height=120px>
-</div>
+This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
 
-# 🚀 FuelMate: Your Quantum Flux Capacitor for Fuel Savings 🌌⛽
+## Getting Started
 
-Welcome aboard the FuelMate spacecraft, where we defy the laws of fuel dynamics and propel you towards a galaxy of savings! Prepare for an interdimensional journey through the cosmos of cost efficiency!
+First, run the development server:
 
-## Frontend Development:
-<ul>
-<li>Designed a user interface (UI) where users can input</li>
-<li>Petrol price per liter</li>
-<li>Mileage of the vehicle (kilometers per liter).</li>
-<li>Distance of travel (in kilometers).</li>
-<li>Add a button to calculate the cost.</li>
-<li>Display the result of the calculation.</li>
-</ul>
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
 
-## Backend Development:
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-Developed the backend logic to handle user inputs and calculate the cost of petrol for the journey.<br>
-This logic will involve taking the inputs (petrol price, mileage, distance) and performing the calculation: cost = (distance / mileage) * petrol price.
+You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
 
-## Contributing: Let's Unravel the Fabric of Reality! 🚀
+This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
 
-FuelMate thrives on collaboration across dimensions! Discovered a quantum anomaly? Have a transcendent feature idea? Don't keep it localized—raise an issue or submit a pull request. Together, we'll rewrite the laws of fuel-saving physics and traverse the infinite cosmos of cost efficiency!
+## Learn More
 
-## Problem❓ that can be solved by this project
+To learn more about Next.js, take a look at the following resources:
 
-We can calculate our fuel economy for our trips and don't need to worry about the calculations..it makes a trip plan more faster...
+- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
 
-## Tech Stack Used👨‍💻
+You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
 
-• Javascript<br>
-• Html<br>
-• Css
+## Deploy on Vercel
 
-## Future Development🧑‍💻
+The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
-Future developing plan is to add fuel price by region wise with a api service and make the user interface more comfortable.
-<!---## Demo👾
-
-<!---(https://github.com/jacobprasanth02/FuelMate/assets/166240794/b755b039-b95d-4f99-836e-172681ea16f3
-
-https://abin-karukappallil.github.io/FuelMate/--->
-## License: Let's Transcend Legal Boundaries! 📝
-
-FuelMate transcends traditional licensing—existing beyond the constraints of space and time. For more interdimensional adventures.
-
-Ready to transcend the boundaries of fuel-saving possibility? Engage the flux capacitor, warp into the unknown, and let FuelMate be your guide through the infinite cosmos of savings! 🌌🚀✨
-
-
+Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
---
+++ app/page.tsx
@@ -0,0 +1,113 @@
+"use client"
+import * as React from 'react';
+import { Input } from '@mui/base/Input';
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+import { useEffect } from 'react';
+import ShimmerButton from '../components/ui/shimmer-button';
+import {IndianRupee} from 'lucide-react';
+
+export default function Page() {
+const [result, setResult] = React.useState('');
+const [fuelPrice, setFuelPrice] = React.useState('');
+const [mileage, setMileage] = React.useState('');
+const [distance, setDistance] = React.useState('');
+const [visible, setVisible] = React.useState(false);
+  useEffect(() => {
+    async function getFuelPrice() {
+      try {
+        const url = 'https://parkplus.io/fuel-price/petrol-diesel-price-in-kottayam';
+        const response = await axios.get(url);
+        const load = cheerio.load(response.data);
+        const priceSpan = load('td[itemprop="description"]').eq(1);
+        if (priceSpan.length) {
+          const fuelPrice = priceSpan.text().trim();
+          setFuelPrice(fuelPrice);
+        } else {
+          throw new Error('not found');
+        }
+      } catch (error) {
+        throw new Error(`Failed`);
+      }
+    }
+    getFuelPrice();
+  },[]);
+ 
+  function calculatePrice(){
+    console.log(fuelPrice, mileage, distance);
+     const price = (parseInt(distance)/parseInt(mileage))*parseInt(fuelPrice);
+     setResult(price.toString());
+     console.log(result)
+  }
+  return (
+    <div className="min-h-screen w-full bg-gradient-to-br from-gray-900 via-gray-900 to-gray-900 relative overflow-hidden">
+      <div className="absolute inset-0 bg-gradient-to-br from-purple-500/10 via-transparent to-emerald-500/10 opacity-50" />
+      <div className="absolute inset-0 bg-[radial-gradient(circle_at_center,rgba(120,50,255,0.15),transparent_50%)]" />
+      <div className="absolute top-45 left-[40vw] w-96 h-80 bg-[radial-gradient(circle,rgba(120,50,255,0.15),transparent_50%)]" />
+<div className="absolute top-0 right-[50vw] w-96 h-80 bg-[radial-gradient(circle,rgba(120,50,255,0.15),transparent_50%)]" />
+<div className="absolute bottom-0 left-0 w-96 h-80 bg-[radial-gradient(circle,rgba(120,50,255,0.15),transparent_50%)]" />
+<div className="absolute bottom-0 right-0 w-96 h-80 bg-[radial-gradient(circle,rgba(120,50,255,0.15),transparent_50%)]" />
+      
+      <div className="relative z-10 container mx-auto px-4 py-20">
+        <h1 className="text-center text-6xl font-bold bg-gradient-to-r from-orange-400 via-orange-500 to-pink-400 text-transparent bg-clip-text">
+          Fuel Mate
+        </h1>
+        
+        <p className="text-gray-400 text-center mt-6 max-w-2xl mx-auto">
+        Your pocket companion for efficient journeys! Unlock the power of smart mileage tracking with simplicity at its core. Say goodbye to fuel-related worries and hello to hassle-free travels. FuelMate, where every mile counts, and your savings soar. Let the journey begin!
+        </p>
+        <div className='flex flex-col justify-center text-slate-200 items-center mt-10 '>
+         
+         <label className='mb-3' htmlFor="fuel price">Current Fuel price</label>
+        <Input
+      className={'dark'}
+      slotProps={{
+        input: {
+          className:
+            'md:w-80 w-60 text-sm font-sans font-normal leading-5 px-3 py-2 rounded-lg shadow-md shadow-slate-100 dark:shadow-slate-900 focus:shadow-outline-purple dark:focus:shadow-outline-purple focus:shadow-lg border border-solid border-slate-300 hover:border-purple-500 dark:hover:border-purple-500 focus:border-purple-500 dark:focus:border-purple-500 dark:border-slate-600 bg-white dark:bg-slate-900 text-slate-900 dark:text-slate-300 focus-visible:outline-0 hover:transform hover:scale-125 transition-transform duration-300',
+        },
+      }}
+      aria-label="Fuel price"
+      value={fuelPrice}
+      disabled
+    />
+      <label className='mb-3 mt-3' htmlFor="fuel price">Enter Milleage of your vehicle</label>
+        <Input
+      className={'dark'}
+      slotProps={{
+        input: {
+          className:
+            'hover:scale-125 transition-transform duration-300 md:w-80 w-60 text-sm font-sans font-normal leading-5 px-3 py-2 rounded-lg shadow-md shadow-slate-100 dark:shadow-slate-900 focus:shadow-outline-purple dark:focus:shadow-outline-purple focus:shadow-lg border border-solid border-slate-300 hover:border-purple-500 dark:hover:border-purple-500 focus:border-purple-500 dark:focus:border-purple-500 dark:border-slate-600 bg-white dark:bg-slate-900 text-slate-900 dark:text-slate-300 focus-visible:outline-0 hover:transform(scale-105)',
+        },
+      }}
+      aria-label="Fuel price"
+      placeholder='km/litre'
+      onChange={(e)=>setMileage(e.target.value)}
+    />
+      <label className='mb-3 mt-3' htmlFor="fuel price">Enter the distance of travel</label>
+        <Input
+      className={'dark'}
+      slotProps={{
+        input: {
+          className:
+            'hover:scale-125 transition-transform duration-300 md:w-80 w-60 text-sm font-sans font-normal leading-5 px-3 py-2 rounded-lg shadow-md shadow-slate-100 dark:shadow-slate-900 focus:shadow-outline-purple dark:focus:shadow-outline-purple focus:shadow-lg border border-solid border-slate-300 hover:border-purple-500 dark:hover:border-purple-500 focus:border-purple-500 dark:focus:border-purple-500 dark:border-slate-600 bg-white dark:bg-slate-900 text-slate-900 dark:text-slate-300 focus-visible:outline-0 hover:transform(scale-105)',
+        },
+      }}
+      aria-label="Fuel price"
+      placeholder='km'
+      onChange={(e)=>setDistance(e.target.value)}
+    />
+  <ShimmerButton className='hover:scale-75 transition-transform duration-300' onClick={()=>{calculatePrice();setVisible(true)}} >Calculate</ShimmerButton>
+
+        </div>
+        <div>
+          {visible && 
+          <div className='flex felx-col justify-center items-center text-center mt-5 text-white text-2xl'>
+           {isNaN(parseInt(result)) ? <p className='text-red-700 text-sm'>ENTER A VALID INPUT</p> :<p className='flex flex-row justify-center items-center text-4xl'><IndianRupee />{parseInt(result)}</p>}
+            </div>}
+        </div>
+      </div>
+    </div>
+  )
+}
+
