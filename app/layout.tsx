c62171cdf36dea2d1af7e3dc3c152bc6771442c7
Abin Thomas <followgamer102@gmail.com>
2025-01-30 17:55:37 +0530

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -14,7 +13,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "FuelMate",
-  description: "Get your expense for trips",
+  description: "Get your expense for tripss",
 };
 
 export default function RootLayout({
